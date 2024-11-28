51bbd7eed2d94c1094493e9205909b615cfac75c
KD5VMF <54245381+KD5VMF@users.noreply.github.com>
2024-11-27 22:02:20 -0600

    coordinator_setup.py

--- coordinator_setup.py
+++ coordinator_setup.py
@@ -2,7 +2,7 @@ import os
 import subprocess
 
 # Ask the user for the virtual environment name
-env_name = input("Enter the virtual environment name to use (default: envName): ").strip() or "envName"
+env_name = input("Enter the virtual environment name to use (default: envMPI): ").strip() or "envName"
 
 # Define the commands to run on the Coordinator Node
 commands = [
